02f5952a06386b9c56c47d973c1435980369e652
BrandonJack9 <bjack@ucsc.edu>
2024-01-21 23:54:15 -0800

    testing orbit control

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -28,7 +28,7 @@ class MyClass {
 // setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
+    /*canvasContainer = $("#canvas-container");
     let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
@@ -40,25 +40,52 @@ function setup() {
     myInstance = new MyClass(VALUE1, VALUE2);
 
     var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    var centerVert = windowHeight / 2;*/
+    createCanvas(710, 400, WEBGL);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
+    /*background(220);    
     // call a method on the instance
     myInstance.myMethod();
 
     // Put drawings here
     var centerHorz = canvasContainer.width() / 2 - 125;
     var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
+    fill(0, 31, 81);
     noStroke();
     rect(centerHorz, centerVert, 250, 250);
     fill(255);
     textStyle(BOLD);
     textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    text("p5*", centerHorz + 10, centerVert + 200);*/
+    background(250);
+    let radius = width * 1.5;
+
+    //drag to move the world.
+    orbitControl();
+
+    normalMaterial();
+    translate(0, 0, -600);
+    for (let i = 0; i <= 12; i++) {
+        for (let j = 0; j <= 12; j++) {
+        push();
+        let a = (j / 12) * PI;
+        let b = (i / 12) * PI;
+        translate(
+            sin(2 * a) * radius * sin(b),
+            (cos(b) * radius) / 2,
+            cos(2 * a) * radius * sin(b)
+        );
+        if (j % 2 === 0) {
+            sphere(30, 30);
+        } else {
+            box(30, 30, 30);
+        }
+        pop();
+        }
+    }
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
