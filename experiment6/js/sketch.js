d49b2bad38a019314cd26d9534a7006eb310fc72
BrandonJack9 <bjack@ucsc.edu>
2024-02-17 00:05:30 -0800

    Revert "trying again"
    This reverts commit 1654a654a712f1ae1a0fd759c2b20f0e3396fc8a.

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -26,10 +26,9 @@ class MyClass {
 }
 
 // setup() function is called once when the program starts
-let song;
 let a;
 let b;
-
+let song;
 
 function preload(){
     song = loadSound("js/starwarsintro.mp3");
@@ -69,14 +68,9 @@ function draw() {
   
   a = a - 0.5;
   b = b - 0.5;
-};
+}
 
-function mouseClicked() {
-    if (song.isPlaying()) {
-      song.pause();
-      noLoop();
-    } else {
-      song.play();
-      loop();
-    }
-  }
+// mousePressed() function is called once after every time a mouse button is pressed
+function mousePressed() {
+    // code to run when mouse is pressed
+}
